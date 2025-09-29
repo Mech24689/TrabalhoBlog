42fd5c2d289a0b3be6360b556554b24a61dc775d
Mech24689 <gabrieltrem102030@gmail.com>
2025-09-29 20:58:12 -0300

    [Gabriel Castro] Criação de header

---
+++ trabalhoBlog/src/components/index.tsx
@@ -0,0 +1,19 @@
+export default function TelaEntrada() {
+  return (
+    <header>
+      <div className="header1">
+        <div className='logo'>HABIT</div>
+        <p>Inicio</p>
+        <p>Páginas</p>
+        <p>Destaques</p>
+        <p>Assinar</p>
+        <p>Admin</p>
+      </div>
+      <div className="header2">
+        <input type="text" id="" />
+        <button>Buscar</button>
+        <button>Entrar</button>
+      </div>
+    </header>
+  );
+}
